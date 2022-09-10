82602b03f74488c8a22ccc5865c8cea5263daa00
Shuvaev_Vadim <shuvaev.v.6@gmail.com>
2022-09-10 20:07:31 +0700

    Changes in compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
 mkdir build
-gcc -Wall -Wextra -Werror -I. -o .\build\Pong.exe .\PongGraphics\main.c -L.\PongGraphics\libs\
--lglfw3 -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32
+gcc -Wall -Wextra -Werror -I. -o .\build\Pong.exe .\PongGraphics\main.c -L.\PongGraphics\libs\ -lglfw3 -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32
