2654e8763aa59c956a81d700cf34b25cfdc49a06
Shuvaev_Vadim <shuvaev_v_6@vk.com>
2023-01-21 19:52:44 +0700

    Some changes in ./compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,4 @@
 if not exist ".\build" (echo "") else (rd /S/Q build)
 mkdir build
 gcc -Wextra -Werror -I. -o .\build\Pong.exe .\PongGraphics\main.c -L.\PongGraphics\libs\Windows\lib-mingw -lglfw3 -lopengl32 -lmingw32 -lgdi32 -luser32 -lkernel32
+echo "Compilation is done."
